53eac95fe773ac41f4368117d84ae4ce7d80b8ac
Carla Daiana Vigil <carvigil21@gmail.com>
2022-05-30 13:12:28 -0300

    TG-38: add validation in delete and corrections

--- src/Components/SuperAdmins/ListItem/ListItem.js
+++ src/Components/SuperAdmins/ListItem/ListItem.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import './listItem.module.css';
+import styles from './listItem.module.css';
 
 function ListItem({ listItem, deleteSuperAdmin }) {
   const handleDelete = () => {
-    let confirm = window.confirm('Are yo sure yo want to delete de Super Admin?');
+    let confirm = window.confirm(
+      `Are you sure you want to delete Super Admin ${listItem.firstName}?`
+    );
     if (confirm) {
       deleteSuperAdmin(listItem._id);
     }
@@ -14,10 +16,12 @@ function ListItem({ listItem, deleteSuperAdmin }) {
       <td>{listItem.lastName}</td>
       <td>{listItem.email}</td>
       <td>
-        <button className="btn delete" onClick={() => handleDelete(listItem._id)}>
+        <button className={styles.btn} onClick={() => handleDelete(listItem._id)}>
           &#10008;
         </button>
-        <button className="btn edit">&#9998;</button>
+        <a className={styles.btn} href={`/super-admins/form?id=${listItem._id}`}>
+          &#9998;
+        </a>
       </td>
     </tr>
   );
--- src/Components/SuperAdmins/index.js
+++ src/Components/SuperAdmins/index.js
@@ -10,25 +10,33 @@ function SuperAdmins() {
 
   useEffect(async () => {
     try {
-      const response = await fetch(process.env.REACT_APP_API_URL);
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/super-admins`);
       const data = await response.json();
       setSuperAdmins(data.data);
-      console.log('Data', data.data);
     } catch (error) {
       console.error(error);
     }
   }, []);
 
   const deleteSuperAdmin = async (_id) => {
-    await fetch(`${process.env.REACT_APP_API_URL}/${_id}`, {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/super-admins/${_id}`, {
       method: 'DELETE'
     });
+    const res = await response.json();
+    if (res.data.error) {
+      window.alert('there was an error');
+    } else {
+      setSuperAdmins([...listSuperAdmins.filter((admin) => admin._id !== _id)]);
+      window.alert(res.message);
+    }
   };
 
   return (
     <section className={styles.container}>
       <h2>SuperAdmins</h2>
-      <button>&#10010; Add Super Admin</button>
+      <a className={styles.btn} href={'/super-admins/form'}>
+        &#10010; Add Super Admin
+      </a>
       <List list={listSuperAdmins} setList={setSuperAdmins} deleteSuperAdmin={deleteSuperAdmin} />
     </section>
   );
