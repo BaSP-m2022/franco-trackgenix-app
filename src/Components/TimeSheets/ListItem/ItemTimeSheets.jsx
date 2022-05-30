cb2d277c9315cb6dc574f521f5f1350ef14851e0
AlejandroSantini <alejandro.j.santini@gmail.com>
2022-05-30 16:38:27 -0300

    TG-41: fix errors

---
+++ src/Components/TimeSheets/ListItem/ItemTimeSheets.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+// import styles from './item-time-sheets.module.css';
+
+const TimeSheetItem = ({ listItem, deleteTimeSheet }) => {
+  console.log('Time sheets list', listItem);
+
+  const handleDelete = () => deleteTimeSheet(listItem._id);
+
+  return (
+    <tr>
+      <td>{listItem._id}</td>
+      <td>{listItem.task}</td>
+      <td>{listItem.totalHours}</td>
+      <td>{listItem.status}</td>
+      <td>{listItem.startDate}</td>
+      <td>{listItem.endDate}</td>
+      <td>{listItem.employeeId}</td>
+      <td>
+        <button onClick={() => handleDelete(listItem._id)}>X</button>
+      </td>
+    </tr>
+  );
+};
+
+export default TimeSheetItem;
--- src/Components/TimeSheets/index.js
+++ src/Components/TimeSheets/index.js
@@ -1,11 +1,44 @@
 import styles from './time-sheets.module.css';
+import { useState, useEffect } from 'react';
+import List from './List/TimeSheetsList.jsx';
 
-function TimeSheets() {
+const TimeSheets = () => {
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState([]);
+  const URL = `${process.env.REACT_APP_API_URL}/time-sheets`;
+
+  // API request to get admins
+  useEffect(async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(URL, {
+        method: 'GET'
+      });
+      const responseJson = await response.json();
+      setList(responseJson.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  // API request to delete admins
+  const deleteTimeSheet = async (_id) => {
+    await fetch(`${process.env.REACT_APP_API_URL}/time-sheets`, { method: 'DELETE' });
+    setList([...list.filter((timeSheet) => timeSheet._id !== _id)]);
+  };
+
+  if (loading) {
+    return <p>Data is loading...</p>;
+  }
+
   return (
     <section className={styles.container}>
       <h2>TimeSheets</h2>
+      <List list={list} setList={setList} deleteTimeSheet={deleteTimeSheet} />
     </section>
   );
-}
+};
 
 export default TimeSheets;
