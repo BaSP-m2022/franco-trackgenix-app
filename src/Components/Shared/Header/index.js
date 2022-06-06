b5d4ae90a5f9ca98ce984de6d23f6f855b4bcabf
Facundo Fernandez <fernandezfacundo14.07@gmail.com>
2022-06-06 10:48:36 -0300

    TG-58: images format changed

--- src/Components/Shared/Header/index.js
+++ src/Components/Shared/Header/index.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import logo1 from './images/logo-1.png';
-import logo2 from './images/logo-2.png';
 import styles from './header.module.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <div id="logo">
-        <Link to={'/home'} className={styles.logo}>
-          {/* <img
-            src={`${process.env.PUBLIC_URL}/assets/images/logo1.png`}
+        <a href={'/home'} className={styles.logo}>
+          {/* <Link to={'/home'} className={styles.logo}> */}
+          <img
+            src={`${process.env.PUBLIC_URL}/assets/images/logo1.svg`}
             alt="Rocket"
             className={styles.rocket}
           />
           <img
-            src={`${process.env.PUBLIC_URL}/assets/images/logo2.png`}
+            src={`${process.env.PUBLIC_URL}/assets/images/logo2.svg`}
             alt="Radium"
             className={styles.radium}
-          /> */}
-          <img src={logo1} alt="Rocket" className={styles.rocket} />
-          <img src={logo2} alt="Radium" className={styles.radium} />
-        </Link>
+          />
+          {/* </Link> */}
+        </a>
       </div>
       <div id="title" className={styles.title}>
         TRACKGENIX
