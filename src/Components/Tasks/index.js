f1d93c67eb04cb37cb06f7d756df0a7a6913b716
AlejandroSantini <alejandro.j.santini@gmail.com>
2022-06-07 15:59:59 -0300

    TG-68: delete older components and implement table

--- src/Components/Tasks/index.js
+++ src/Components/Tasks/index.js
@@ -1,45 +1,50 @@
 import { useEffect, useState } from 'react';
-import List from './List/List';
 import styles from './tasks.module.css';
+import Table from '../Shared/Table';
 
 function Tasks() {
-  const [list, setList] = useState([]);
+  const [dataTable, setDataTable] = useState([]);
 
   useEffect(async () => {
     try {
       const response = await fetch(`${process.env.REACT_APP_API_URL}/tasks`);
       const responseJson = await response.json();
-      setList(responseJson.data);
+      console.log(responseJson.data);
+      setDataTable(responseJson.data);
     } catch (error) {
       console.error(error);
     }
   }, []);
+
   const deleteItem = async (id) => {
-    let confirm = window.confirm('Do you wish to delete the task?');
-    if (confirm) {
-      try {
-        await fetch(`${process.env.REACT_APP_API_URL}/tasks/${id}`, {
-          method: 'DELETE'
-        });
-        const result = list.filter((task) => task._id !== id);
-        setList(result);
-      } catch (error) {
-        console.log(error);
-      }
+    try {
+      await fetch(`${process.env.REACT_APP_API_URL}/tasks/${id}`, {
+        method: 'DELETE'
+      });
+      const result = dataTable.filter((task) => task._id !== id);
+      setDataTable(result);
+    } catch (error) {
+      console.log(error);
     }
   };
+
+  const column = [
+    { heading: 'Id', value: '_id' },
+    { heading: 'Description', value: 'description' },
+    { heading: 'Worked Hours', value: 'workedHours' },
+    { heading: 'Project', value: 'projectId.name' },
+    { heading: 'Date', value: 'date' }
+  ];
+
+  const entity = 'tasks';
 
   return (
     <section className={styles.container}>
       <h2 className={styles.h2_task}>Tasks</h2>
       <div>
-        <List list={list} setList={setList} deleteItem={deleteItem} />
+        <Table data={dataTable} column={column} deleteItem={deleteItem} entity={entity} />
       </div>
-      <div className={styles.btn_div}>
-        <a href="/tasks/form" className={styles.button}>
-          &#10010; Add new task
-        </a>
-      </div>
+      <div className={styles.btn_div}></div>
     </section>
   );
 }
