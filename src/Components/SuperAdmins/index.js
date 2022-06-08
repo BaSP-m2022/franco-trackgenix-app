2ee28b00fbefbadb17646f4fa065ae4c62a6e0dd
Andres Geary <gearyandres@gmail.com>
2022-06-07 21:59:50 -0300

    TG-66: List finished. All styles done

--- src/Components/SuperAdmins/index.js
+++ src/Components/SuperAdmins/index.js
@@ -1,40 +1,96 @@
 import { useEffect, useState } from 'react';
 import styles from './super-admins.module.css';
-import List from './List/List';
+import Table from '../Shared/Table';
+import LoadingScreen from '../Shared/LoadingScreen';
+import Modal from '../Shared/Modal';
+import Button from '../Shared/Button';
+import Search from '../Shared/Search-bar';
 
 function SuperAdmins() {
-  const [listSuperAdmins, setSuperAdmins] = useState([]);
+  const [dataTable, setDataTable] = useState([]);
+  const [untouchedData, setUntouchedData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [toBeDeleted, setToBeDeleted] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const entity = 'Super Admins';
+  const [search, setSearch] = useState();
+
+  const setSearchQuery = (value) => {
+    setSearch(value);
+    setDataTable(
+      untouchedData.filter((superAdmin) => superAdmin.firstName.toLowerCase().includes(value))
+    );
+  };
+
+  const openModal = (id) => {
+    setToBeDeleted(id);
+    setIsOpen(true);
+  };
 
   useEffect(async () => {
     try {
+      setLoading(true);
       const response = await fetch(`${process.env.REACT_APP_API_URL}/super-admins`);
       const data = await response.json();
-      setSuperAdmins(data.data);
+      setDataTable(data.data);
+      setUntouchedData(data.data);
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
   }, []);
 
-  const deleteSuperAdmin = async (_id) => {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/super-admins/${_id}`, {
+  const deleteSuperAdmin = async (id) => {
+    setLoading(true);
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/super-admins/${id}`, {
       method: 'DELETE'
     });
     const res = await response.json();
     if (res.data.error) {
-      window.alert('there was an error');
+      setIsOpen(false);
+      setLoading(false);
     } else {
-      setSuperAdmins([...listSuperAdmins.filter((admin) => admin._id !== _id)]);
-      window.alert(res.message);
+      setDataTable([...dataTable.filter((admin) => admin._id !== id)]);
+      setIsOpen(false);
+      setLoading(false);
     }
   };
 
+  const column = [
+    { heading: 'Id', value: '_id' },
+    { heading: 'First Name', value: 'firstName' },
+    { heading: 'Last Name', value: 'lastName' },
+    { heading: 'Email', value: 'email' },
+    { heading: 'Password', value: 'password' }
+  ];
+  if (loading) {
+    return <LoadingScreen />;
+  }
   return (
-    <section className={styles.container}>
+    <section className={styles.containerSuperAdmin}>
+      <Modal
+        isOpen={isOpen}
+        handleClose={() => setIsOpen(!isOpen)}
+        modalTitle={'Delete Super Admin'}
+      >
+        <p>Do you Wish to delete the Super Admin?</p>
+        <div>
+          <Button type={'delete'} handler={() => deleteSuperAdmin(toBeDeleted)} text={'Yes'} />
+          <Button handler={() => setIsOpen(!isOpen)} text={'No'} />
+        </div>
+      </Modal>
       <h2>SuperAdmins</h2>
-      <a className={styles.btn} href={'/super-admins/form'}>
-        &#10010; Add Super Admin
-      </a>
-      <List list={listSuperAdmins} setList={setSuperAdmins} deleteSuperAdmin={deleteSuperAdmin} />
+      <div className={styles.searchDiv}>
+        <Search
+          searchQuery={search}
+          setSearchQuery={setSearchQuery}
+          placeholder={'Insert Super Admin Name'}
+        />
+      </div>
+      <div className={styles.buttonDiv}>
+        <Button link={'/super-admins/form'} text={'Add Super Admin'} />
+      </div>
+      <Table data={dataTable} column={column} deleteItem={openModal} entity={entity} />
     </section>
   );
 }
