53159d1253dee93bc2a74f80378a1560da4dbe60
GuidoCerioni <guidocerioni98@gmail.com>
2022-06-14 23:44:11 -0300

    TG-86: add thunk and reducer

--- src/redux/tasks/reducer.js
+++ src/redux/tasks/reducer.js
@@ -14,12 +14,14 @@ export const tasksReducer = (state = initialStore, actions) => {
         ...state,
         loading: true
       };
+
     case CONSTANTS.GET_TASKS_SUCCESS:
       return {
         ...state,
         list: actions.payload,
         loading: false
       };
+
     case CONSTANTS.GET_TASKS_ERROR:
       return {
         ...state,
@@ -32,28 +34,80 @@ export const tasksReducer = (state = initialStore, actions) => {
         ...state,
         loading: true
       };
+
     case CONSTANTS.DELETE_TASK_SUCCESS:
       return {
         ...state,
-        list: state.list.filter((tasks) => tasks._id !== actions.payload._id),
+        list: state.list.filter((task) => task._id !== actions.payload._id),
         loading: false
       };
+
     case CONSTANTS.DELETE_TASK_ERROR:
       return {
         ...state,
         error: actions.payload,
         loading: false
       };
-    case CONSTANTS.CLEAR_ERROR:
+
+    case CONSTANTS.PUT_TASK_LOADING:
       return {
         ...state,
-        error: ''
+        loading: true
       };
+
+    case CONSTANTS.PUT_TASK_SUCCESS:
+      return {
+        ...state,
+        list: state.list.map((item) => {
+          if (item._id === actions.payload._id) {
+            return actions.payload;
+          }
+          return item;
+        }),
+        task: {},
+        error: '',
+        loading: false
+      };
+
+    case CONSTANTS.PUT_TASK_ERROR:
+      return {
+        ...state,
+        error: actions.payload,
+        loading: false
+      };
+
+    case CONSTANTS.ADD_TASK_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+
+    case CONSTANTS.ADD_TASK_SUCCESS:
+      return {
+        ...state,
+        list: [...state.list, actions.payload],
+        error: '',
+        loading: false
+      };
+
+    case CONSTANTS.ADD_TASK_ERROR:
+      return {
+        ...state,
+        error: actions.payload,
+        loading: false
+      };
+
     case CONSTANTS.SET_TASK:
       return {
         ...state,
         task: actions.payload ? state.list.find((task) => task._id === actions.payload) : {},
         loading: false
+      };
+
+    case CONSTANTS.CLEAR_ERROR:
+      return {
+        ...state,
+        error: ''
       };
 
     default:
