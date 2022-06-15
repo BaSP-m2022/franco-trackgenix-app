61e92154de72a18622a0e2bbfd1986038ac2026a
AlejandroSantini <alejandro.j.santini@gmail.com>
2022-06-14 21:02:19 -0300

    TG-87: remove old logic and add data of redux

--- src/components/TimeSheet/index.js
+++ src/components/TimeSheet/index.js
@@ -1,40 +1,48 @@
-import { useState, useEffect } from 'react';
-import styles from './form.module.css';
+import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { postTimeSheet, putTimeSheet } from '../../redux/timeSheets/thunks';
+import { clearError } from '../../redux/timeSheets/actions';
 import Modal from '../Shared/Modal';
 import Button from '../Shared/Button';
-import { useHistory } from 'react-router-dom';
-import SelectDropdown from '../Shared/SelectDropdown';
 import Input from '../Shared/Input';
-import { Children } from 'react';
+import LoadingScreen from '../Shared/LoadingScreen';
+import SelectDropdown from '../Shared/SelectDropdown';
 
-function TimeSheetForm() {
+import styles from './form.module.css';
+
+const TimeSheetForm = () => {
   const [taskValue, setTaskValue] = useState([]);
   const [totalHoursValue, setTotalHoursValue] = useState('');
   const [statusValue, setStatusValue] = useState('');
   const [startDateValue, setStartDateValue] = useState('');
   const [endDateValue, setEndDateValue] = useState('');
   const [employeeIdValue, setEmployeeIdValue] = useState({});
+
   const [employeeOptions, setEmployeeOptions] = useState([]);
   const [tasksOptions, setTasksOptions] = useState([]);
-  const [error, setError] = useState('');
-  const [redirect, setRedirect] = useState(false);
-  const [isLoading, setLoading] = useState(false);
-  const [modalMessage, setModalMessage] = useState('');
-  const [showModal, setShowModal] = useState(false);
-  const [isPutRequest, setIsPutRequest] = useState(false);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [requestType, setRequestType] = useState('POST');
+  const [modalText, setModalText] = useState('');
+  const [modalTitle, setModalTitle] = useState('');
 
   const history = useHistory();
 
-  const routeChange = () => {
-    let path = `/time-sheets`;
-    history.push(path);
-  };
+  const dispatch = useDispatch();
+
+  const employees = useSelector((state) => state.employees.list);
+  const tasks = useSelector((state) => state.tasks.list);
+  const timeSheet = useSelector((state) => state.timeSheets.timeSheet);
+  const loading = useSelector((state) => state.timeSheets.loading);
+  const error = useSelector((state) => state.timeSheets.error);
 
   const statusOption = [
     { value: 'To do', label: 'To do' },
     { value: 'In progress', label: 'In progress' },
     { value: 'Done', label: 'Done' }
   ];
+
   const onChangeStatusInput = (event) => {
     setStatusValue(event.target.value);
   };
@@ -50,199 +58,166 @@ function TimeSheetForm() {
     setEmployeeIdValue(event.target.value);
   };
 
-  useEffect(async () => {
-    try {
-      setLoading(true);
-      const employeesData = await fetch(`${process.env.REACT_APP_API_URL}/employees/`);
-      const employees = await employeesData.json();
-      setEmployeeOptions(
-        employees.data.map(({ _id, firstName, lastName }) => ({
-          value: _id,
-          label: `${firstName} ${lastName}`
-        }))
-      );
-      const tasksData = await fetch(`${process.env.REACT_APP_API_URL}/tasks/`);
-      const tasks = await tasksData.json();
-      setTasksOptions(
-        tasks.data.map(({ _id, description }) => ({ value: _id, label: description }))
-      );
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
+  useEffect(() => {
+    setEmployeeOptions(
+      employees.map(({ _id, firstName, lastName }) => ({
+        value: _id,
+        label: `${firstName} ${lastName}`
+      }))
+    );
+    setTasksOptions(tasks.map(({ _id, description }) => ({ value: _id, label: description })));
   }, []);
 
   useEffect(() => {
-    setLoading(true);
-    const params = new URLSearchParams(window.location.search);
-    const timeSheetId = params.get('id');
-    if (timeSheetId) {
-      setIsPutRequest(true);
-      fetch(`${process.env.REACT_APP_API_URL}/time-sheets/${timeSheetId}`)
-        .then((response) => {
-          if (response.status !== 200) {
-            return response.json().then(({ message }) => {
-              throw new Error(message);
-            });
-          }
-          return response.json();
-        })
-        .then((response) => {
-          setTaskValue(response.data.tasks);
-          setTotalHoursValue(response.data.totalHours);
-          setStatusValue(response.data.status);
-          setStartDateValue(response.data.startDate.split('T')[0]);
-          setEndDateValue(response.data.endDate.split('T')[0]);
-          setEmployeeIdValue(response.data.employeeId);
-        })
-        .catch((error) => {
-          setError(error.toString());
-        })
-        .finally(() => setLoading(false));
+    if (timeSheet._id) {
+      setTaskValue(timeSheet.taskValue);
+      setTotalHoursValue(timeSheet.totalHoursValue);
+      setStatusValue(timeSheet.startDateValue);
+      setStartDateValue(timeSheet.startDateValue);
+      setEndDateValue(timeSheet.endDateValue);
+      setEmployeeIdValue(timeSheet.employeeIdValue);
+      setRequestType('PUT');
     }
-  }, []);
+  }, [error]);
 
-  const onSubmit = async (event) => {
-    setLoading(true);
-    event.preventDefault();
-    const params = new URLSearchParams(window.location.search);
-    const timeSheetId = params.get('id');
-    setModalMessage('Timesheet created correctly!');
-    setRedirect(true);
-    let url = `${process.env.REACT_APP_API_URL}/time-sheets/`;
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        tasks: taskValue,
-        totalHours: totalHoursValue,
-        status: statusValue,
-        startDate: startDateValue,
-        endDate: endDateValue,
-        employeeId: employeeIdValue
-      })
-    };
-    if (timeSheetId) {
-      setModalMessage('Timesheet edited correctly!');
-      setRedirect(true);
-      options.method = 'PUT';
-      url = `${process.env.REACT_APP_API_URL}/time-sheets/${timeSheetId}`;
-    }
-    try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-      if (data.error) {
-        setModalMessage('There was an error');
-        setRedirect(false);
-      }
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setShowModal(true);
+  const routeChange = () => {
+    let path = `/time-sheets`;
+    history.push(path);
+  };
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  if (loading) {
+    return (
+      <div className={styles.loading}>
+        <LoadingScreen />
+      </div>
+    );
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const body = JSON.stringify({
+      tasks: taskValue,
+      totalHours: totalHoursValue,
+      status: statusValue,
+      startDate: startDateValue,
+      endDate: endDateValue,
+      employeeId: employeeIdValue
+    });
+    if (requestType === 'PUT') {
+      setModalTitle('TimeSheet Updated');
+      setModalText('TimeSheet has been updated');
+      dispatch(putTimeSheet(timeSheet._id, body));
+      openModal();
+    } else {
+      setModalTitle('TimeSheet Created');
+      setModalText('TimeSheet has been created');
+      dispatch(postTimeSheet(body));
+      openModal();
     }
   };
 
   return (
     <div className={styles.container}>
-      <h2>Time Sheets Form</h2>
-      <form onSubmit={onSubmit}>
-        <div className={styles.select}>
-          <SelectDropdown
-            name="Task"
-            onChange={onChangeTaskInput}
-            value={taskValue}
-            required="true"
-            type="text"
-            options={tasksOptions}
-            disabled={isLoading}
-          />
-        </div>
-
-        <div className={styles.input}>
-          <Input
-            name="Total Hours"
-            placeholder="Total Hours"
-            type="number"
-            value={totalHoursValue}
-            onChange={setTotalHoursValue}
-            disabled={isLoading}
-          />
-        </div>
-        <div className={styles.select}>
-          <SelectDropdown
-            name="status"
-            onChange={onChangeStatusInput}
-            value={statusValue}
-            props="status"
-            required="true"
-            type="status"
-            options={statusOption}
-            disabled={isLoading}
-          />
-        </div>
-        <div className={styles.input}>
-          <Input
-            name="Start Date"
-            placeholder="Start Date"
-            type="date"
-            value={startDateValue}
-            onChange={setStartDateValue}
-            disabled={isLoading}
-          />
-        </div>
-        <div className={styles.input}>
-          <Input
-            name="End Date"
-            placeholder="End Date"
-            type="date"
-            value={endDateValue}
-            onChange={setEndDateValue}
-            disabled={isLoading}
-          />
-        </div>
-        <div className={styles.select}>
-          <SelectDropdown
-            name="Employees"
-            onChange={onChangeEmployeeIdInput}
-            value={employeeIdValue}
-            props="employeeId"
-            required="true"
-            type="text"
-            options={employeeOptions}
-            disabled={isLoading}
-          />
-        </div>
-        <div className={styles.buttomDiv}>
-          <Button
-            text={isPutRequest ? 'Update Time-Sheet' : 'save Time-Sheet'}
-            handler={onSubmit}
-          />
-          <Button text={'Return'} handler={routeChange} />
-        </div>
-        <Modal
-          isOpen={showModal}
-          modalTitle={modalMessage}
-          handleClose={
-            redirect
-              ? showModal
-              : () => {
-                  setShowModal(!showModal);
-                }
-          }
-          setModalMessage={Children}
-        >
-          <div>
-            <Button text={'Ok'} handler={redirect ? routeChange : () => setShowModal(!showModal)} />
+      <h3 className={styles.h3}>{requestType === 'PUT' ? 'Update Admin' : 'Add Admin'}</h3>
+      <form className={styles.form}>
+        <div className={styles.inputs}>
+          <div className={styles.select}>
+            <SelectDropdown
+              name="Task"
+              onChange={onChangeTaskInput}
+              value={taskValue}
+              required="true"
+              type="text"
+              options={tasksOptions}
+              disabled={loading}
+            />
           </div>
-        </Modal>
-        <div className={styles.error}>{error}</div>
+          <div className={styles.input}>
+            <Input
+              name="Total Hours"
+              placeholder="Total Hours"
+              type="number"
+              value={totalHoursValue}
+              onChange={setTotalHoursValue}
+              disabled={loading}
+            />
+          </div>
+          <div className={styles.select}>
+            <SelectDropdown
+              name="status"
+              onChange={onChangeStatusInput}
+              value={statusValue}
+              props="status"
+              required="true"
+              type="status"
+              options={statusOption}
+              disabled={loading}
+            />
+          </div>
+          <div className={styles.input}>
+            <Input
+              name="Start Date"
+              placeholder="Start Date"
+              type="date"
+              value={startDateValue}
+              onChange={setStartDateValue}
+              disabled={loading}
+            />
+          </div>
+          <div className={styles.input}>
+            <Input
+              name="End Date"
+              placeholder="End Date"
+              type="date"
+              value={endDateValue}
+              onChange={setEndDateValue}
+              disabled={loading}
+            />
+          </div>
+          <div className={styles.select}>
+            <SelectDropdown
+              name="Employees"
+              onChange={onChangeEmployeeIdInput}
+              value={employeeIdValue}
+              props="employeeId"
+              required="true"
+              type="text"
+              options={employeeOptions}
+              disabled={loading}
+            />
+          </div>
+          <div className={styles.buttonDiv}>
+            <Button
+              text="Return"
+              handler={() => {
+                dispatch(clearError());
+                routeChange();
+              }}
+            />
+            <Button handler={handleSubmit} text={requestType === 'PUT' ? 'Update' : 'Save'} />
+            <Modal
+              modalTitle={error ? 'error' : modalTitle}
+              isOpen={isOpen}
+              handleClose={closeModal}
+            >
+              <p className={styles.message}>{error ? error : modalText}</p>
+              <div>
+                <Button text="OK" handler={!error ? routeChange : closeModal} />
+              </div>
+            </Modal>
+          </div>
+        </div>
       </form>
     </div>
   );
-}
+};
 
 export default TimeSheetForm;
