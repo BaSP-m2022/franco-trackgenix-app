7e745e0e0658f936614fa0bb23e6ef6e848899c1
GuidoCerioni <guidocerioni98@gmail.com>
2022-06-24 02:02:45 -0300

    TG-110: profile layout style changes

--- src/components/Shared/Profile/index.js
+++ src/components/Shared/Profile/index.js
@@ -44,6 +44,9 @@ const schema = Joi.object({
     .email({ tlds: { allow: false } })
     .message('Your email must be a valid email')
     .required(),
+  dateOfBirth: Joi.date().max(moreThan18).message('You must be more than 18 years old').required()
+});
+const schemaPassword = Joi.object({
   oldPassword: Joi.string()
     .min(8)
     .message('Password must have between 8 and 12 characters')
@@ -67,8 +70,7 @@ const schema = Joi.object({
   rpassword: Joi.any()
     .equal(Joi.ref('password'))
     .required()
-    .messages({ 'any.only': 'Passwords must match' }),
-  dateOfBirth: Joi.date().max(moreThan18).message('You must be more than 18 years old').required()
+    .messages({ 'any.only': 'Passwords must match' })
 });
 
 const Profile = () => {
@@ -77,9 +79,18 @@ const Profile = () => {
     defaultValues: {
       firstName: '',
       lastName: '',
-      dateOfBirth: '',
-      dni: '',
       email: '',
+      dateOfBirth: '',
+      dni: ''
+    }
+  });
+  const {
+    handleSubmit: handleSubmitPassword,
+    control: controlPassword,
+    setValue: setValuePassword
+  } = useForm({
+    resolver: joiResolver(schemaPassword),
+    defaultValues: {
       password: '',
       rpassword: '',
       oldPassword: ''
@@ -92,7 +103,6 @@ const Profile = () => {
   const [msg, setMsg] = useState('');
   const [modalTitle, setModalTitle] = useState('');
   const [isOpen, setIsOpen] = useState(false);
-  const [changePassword, setChangePassword] = useState(false);
   const [oldPasswordError, setOldPasswordError] = useState('');
 
   const employee = useSelector((state) => state.employees.employee);
@@ -106,9 +116,9 @@ const Profile = () => {
       setValue('dateOfBirth', employee.dateOfBirth.slice(0, 10));
       setValue('dni', employee.dni);
       setValue('email', employee.email);
-      setValue('password', employee.password);
-      setValue('rpassword', employee.password);
-      setValue('oldPassword', employee.password);
+      setValuePassword('password', '');
+      setValuePassword('rpassword', '');
+      setValuePassword('oldPassword', '');
     }
   }, [employee]);
 
@@ -125,28 +135,36 @@ const Profile = () => {
     history.push(path);
   };
 
-  const handleChangePassword = () => {
-    setChangePassword(true);
-    setValue('password', '');
-    setValue('rpassword', '');
-    setValue('oldPassword', '');
+  const onSubmit = (data) => {
+    const body = JSON.stringify({
+      firstName: capitalizeFirstLetter(data.firstName),
+      lastName: capitalizeFirstLetter(data.lastName),
+      dni: data.dni,
+      email: data.email,
+      dateOfBirth: data.dateOfBirth,
+      password: employee.password
+    });
+    dispatch(putEmployee(employee._id, body));
+    setOldPasswordError('');
+    setModalTitle('Profile updated');
+    setMsg('You have updated your profile successfully!');
+    openModal();
   };
 
-  const onSubmit = (data) => {
+  const onSubmitPassword = (data) => {
     if (data.oldPassword === employee.password) {
       const body = JSON.stringify({
-        firstName: capitalizeFirstLetter(data.firstName),
-        lastName: capitalizeFirstLetter(data.lastName),
-        dni: data.dni,
-        email: data.email,
         password: data.password,
-        dateOfBirth: data.dateOfBirth
+        firstName: capitalizeFirstLetter(employee.firstName),
+        lastName: capitalizeFirstLetter(employee.lastName),
+        dni: employee.dni,
+        email: employee.email,
+        dateOfBirth: employee.dateOfBirth
       });
       dispatch(putEmployee(employee._id, body));
       setOldPasswordError('');
-      setChangePassword(false);
-      setModalTitle('Profile updated');
-      setMsg('You have updated your profile successfully!');
+      setModalTitle('Password updated');
+      setMsg('You have updated your password successfully!');
       openModal();
     } else {
       setOldPasswordError('Password is incorrect');
@@ -173,138 +191,142 @@ const Profile = () => {
           />
         </div>
       </Modal>
-      <h2 className={styles.subtitle}>Profile</h2>
-      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          control={control}
-          name="firstName"
-          render={({ field: { value, onChange }, fieldState: { error } }) => (
-            <Input
-              value={value}
-              onChange={onChange}
-              error={error?.message}
-              name="First name"
-              placeholder="First name"
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name="lastName"
-          render={({ field: { value, onChange }, fieldState: { error } }) => (
-            <Input
-              name="Last name"
-              value={value}
-              placeholder="Last name"
-              onChange={onChange}
-              error={error?.message}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name="email"
-          render={({ field: { value, onChange }, fieldState: { error } }) => (
-            <Input
-              name="Email"
-              value={value}
-              placeholder="Email"
-              onChange={onChange}
-              error={error?.message}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name="dateOfBirth"
-          render={({ field: { value, onChange }, fieldState: { error } }) => (
-            <Input
-              type="date"
-              name="Date of birth"
-              value={value}
-              placeholder="Date of birth"
-              onChange={onChange}
-              error={error?.message}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name="dni"
-          render={({ field: { value, onChange }, fieldState: { error } }) => (
-            <Input
-              type="number"
-              name="DNI"
-              value={value}
-              placeholder="DNI"
-              onChange={onChange}
-              error={error?.message}
-            />
-          )}
-        />
-        <h2 className={styles.subtitle}>Security</h2>
-        {!changePassword ? (
-          <div className={styles.changePasswordButton}>
-            <Button text="Change password" handler={handleChangePassword} />
-          </div>
-        ) : (
-          <div className={styles.passwordInputs}>
-            <Controller
-              control={control}
-              name="oldPassword"
-              render={({ field: { value, onChange }, fieldState: { error } }) => (
-                <Input
-                  type="password"
-                  name="Old password"
-                  value={value}
-                  placeholder="Old password"
-                  onChange={onChange}
-                  error={oldPasswordError ? oldPasswordError : error?.message}
-                />
-              )}
-            />
-            <Controller
-              control={control}
-              name="password"
-              render={({ field: { value, onChange }, fieldState: { error } }) => (
-                <Input
-                  type="password"
-                  name="New password"
-                  value={value}
-                  placeholder="New password"
-                  onChange={onChange}
-                  error={error?.message}
-                />
-              )}
-            />
-            <Controller
-              control={control}
-              name="rpassword"
-              render={({ field: { value, onChange }, fieldState: { error } }) => (
-                <Input
-                  type="password"
-                  name="Repeat password"
-                  value={value}
-                  placeholder="Repeat password"
-                  onChange={onChange}
-                  error={error?.message}
-                />
-              )}
-            />
-          </div>
-        )}
-      </form>
 
-      <div className={styles.buttonsContainer}>
-        <Button
-          text="Return"
-          handler={() => {
-            dispatch(clearError());
-            routeChange();
-          }}
-        />
-        <Button text="Edit profile" className={styles.button} handler={handleSubmit(onSubmit)} />
-      </div>
+      <form className={`${styles.profile} ${styles.form}`} onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.inputsContainer}>
+          <h2 className={styles.subtitle}>Profile</h2>
+          <Controller
+            control={control}
+            name="firstName"
+            render={({ field: { value, onChange }, fieldState: { error } }) => (
+              <Input
+                value={value}
+                onChange={onChange}
+                error={error?.message}
+                name="First name"
+                placeholder="First name"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="lastName"
+            render={({ field: { value, onChange }, fieldState: { error } }) => (
+              <Input
+                name="Last name"
+                value={value}
+                placeholder="Last name"
+                onChange={onChange}
+                error={error?.message}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { value, onChange }, fieldState: { error } }) => (
+              <Input
+                name="Email"
+                value={value}
+                placeholder="Email"
+                onChange={onChange}
+                error={error?.message}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="dateOfBirth"
+            render={({ field: { value, onChange }, fieldState: { error } }) => (
+              <Input
+                type="date"
+                name="Date of birth"
+                value={value}
+                placeholder="Date of birth"
+                onChange={onChange}
+                error={error?.message}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="dni"
+            render={({ field: { value, onChange }, fieldState: { error } }) => (
+              <Input
+                type="number"
+                name="DNI"
+                value={value}
+                placeholder="DNI"
+                onChange={onChange}
+                error={error?.message}
+              />
+            )}
+          />
+        </div>
+        <div className={styles.buttonsContainer}>
+          <Button
+            text="Return"
+            handler={() => {
+              dispatch(clearError());
+              routeChange();
+            }}
+          />
+          <Button text="Edit profile" className={styles.button} handler={handleSubmit(onSubmit)} />
+        </div>
+      </form>
+      <form
+        className={`${styles.security} ${styles.form}`}
+        onSubmit={handleSubmitPassword(onSubmitPassword)}
+      >
+        <div className={styles.inputsContainer}>
+          <h2 className={styles.subtitle}>Security</h2>
+          <Controller
+            control={controlPassword}
+            name="oldPassword"
+            render={({ field: { value, onChange }, fieldState: { error } }) => (
+              <Input
+                type="password"
+                name="Old password"
+                value={value}
+                placeholder="Old password"
+                onChange={onChange}
+                error={oldPasswordError ? oldPasswordError : error?.message}
+              />
+            )}
+          />
+          <Controller
+            control={controlPassword}
+            name="password"
+            render={({ field: { value, onChange }, fieldState: { error } }) => (
+              <Input
+                type="password"
+                name="New password"
+                value={value}
+                placeholder="New password"
+                onChange={onChange}
+                error={error?.message}
+              />
+            )}
+          />
+          <Controller
+            control={controlPassword}
+            name="rpassword"
+            render={({ field: { value, onChange }, fieldState: { error } }) => (
+              <Input
+                type="password"
+                name="Repeat password"
+                value={value}
+                placeholder="Repeat password"
+                onChange={onChange}
+                error={error?.message}
+              />
+            )}
+          />
+        </div>
+        <div className={styles.changePasswordButton}>
+          <Button text="Change password" handler={handleSubmitPassword(onSubmitPassword)} />
+        </div>
+      </form>
     </section>
   );
 };
