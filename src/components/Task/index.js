8bd33c88c2ab9c2d6e674f3b9d42a9d7bed78c40
AlejandroSantini <alejandro.j.santini@gmail.com>
2022-06-26 11:21:39 -0300

    TG-107: fix date error

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -35,7 +35,7 @@ const TaskForm = () => {
     description: Joi.string().min(3).max(50).required(),
     workedHours: Joi.number().min(1).required(),
     projectId: Joi.string().required(),
-    date: Joi.date().required()
+    date: Joi.date().min(Date()).required()
   });
 
   const { handleSubmit, setValue, control } = useForm({
