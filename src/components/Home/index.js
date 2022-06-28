ddfb0a4e208df1b040ed1c65aca7f7fab03699c9
Hernan Cazeneuve <hernancazeneuve@gmail.com>
2022-06-28 10:36:55 -0300

    TG-118: firebase setup applied to home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,13 @@
 import styles from './home.module.css';
+import { login } from 'redux/auth/thunks';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 function Home() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(login({ email: 'andres@gmail.com', password: '1234567a' }));
+  }, []);
   return (
     <section className={styles.container}>
       <h2>Home</h2>
