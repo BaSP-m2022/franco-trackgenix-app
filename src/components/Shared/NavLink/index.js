961caac87c7971aa29aacf1eaa48d67c95b25a9e
Laura Brussa <mlaurafbrussa@gmail.com>
2022-06-30 13:40:03 -0300

    TG-126: continue with NavLink shared component

--- src/components/Shared/NavLink/index.js
+++ src/components/Shared/NavLink/index.js
@@ -1,41 +1,119 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './NavLink.module.css';
 import { NavLink } from 'react-router-dom';
 
 const NavLinks = () => {
+  const [loginUserRole, setLoginUserRole] = useState('');
+  const logIn = (logInUser) => {
+    setLoginUserRole(logInUser);
+  };
+
+  useEffect(() => {
+    logIn();
+  }, []);
+
   return (
-    <ul className={styles.routes}>
-      <li className={styles.items}>
+    <div>
+      {(() => {
+        switch (loginUserRole) {
+          case 'employees':
+            return (
+              <ul className={styles.routes}>
+                <li className={styles.items}>
+                  <NavLink to={'home'} className={styles.links}>
+                    Home
+                  </NavLink>
+                </li>
+                <li className={styles.items}>
+                  <NavLink to={'/time-sheets'} className={styles.links}>
+                    Timesheets
+                  </NavLink>
+                </li>
+              </ul>
+            );
+          case 'admin':
+            return (
+              <ul className={styles.routes}>
+                <li className={styles.items}>
+                  <NavLink to={'home'} className={styles.links}>
+                    Home
+                  </NavLink>
+                </li>
+                <li className={styles.items}>
+                  <NavLink to={'/admins'} className={styles.links}>
+                    Admins
+                  </NavLink>
+                </li>
+                <li className={styles.items}>
+                  <NavLink to={'/employees'} className={styles.links}>
+                    Employees
+                  </NavLink>
+                </li>
+                <li className={styles.items}>
+                  <NavLink to={'/projects'} className={styles.links}>
+                    Projects
+                  </NavLink>
+                </li>
+                <li className={styles.items}>
+                  <NavLink to={'/time-sheets'} className={styles.links}>
+                    Timesheets
+                  </NavLink>
+                </li>
+              </ul>
+            );
+          case 'superAdmin':
+            return (
+              <ul className={styles.routes}>
+                <li className={styles.items}>
+                  <NavLink to={'home'} className={styles.links}>
+                    Home
+                  </NavLink>
+                </li>
+              </ul>
+            );
+          default:
+            return (
+              <ul className={styles.routes}>
+                <li className={styles.items}>
+                  <NavLink to={'home'} className={styles.links}>
+                    Home
+                  </NavLink>
+                </li>
+              </ul>
+            );
+        }
+      })()}
+      {/* <li className={styles.items}>
         <NavLink to={'home'} className={styles.links}>
-          Home
+        Home
         </NavLink>
-      </li>
-      <li className={styles.items}>
+        </li>
+        <li className={styles.items}>
         <NavLink to={'/admins'} className={styles.links}>
-          Admins
+        Admins
         </NavLink>
-      </li>
-      <li className={styles.items}>
+        </li>
+        <li className={styles.items}>
         <NavLink to={'/super-admins'} className={styles.links}>
           Super admins
-        </NavLink>
-      </li>
-      <li className={styles.items}>
-        <NavLink to={'/employees'} className={styles.links}>
+          </NavLink>
+          </li>
+          <li className={styles.items}>
+          <NavLink to={'/employees'} className={styles.links}>
           Employees
-        </NavLink>
-      </li>
-      <li className={styles.items}>
+          </NavLink>
+          </li>
+          <li className={styles.items}>
         <NavLink to={'/projects'} className={styles.links}>
-          Projects
+        Projects
         </NavLink>
-      </li>
-      <li className={styles.items}>
+        </li>
+        <li className={styles.items}>
         <NavLink to={'/time-sheets'} className={styles.links}>
-          Timesheets
+        Timesheets
         </NavLink>
-      </li>
-    </ul>
+      </li> */}
+    </div>
   );
 };
 
