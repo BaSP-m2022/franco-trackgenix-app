ce60d120cd72e9b266b9ce43130cf94996ccd9c3
GuidoCerioni <guidocerioni98@gmail.com>
2022-07-05 19:29:11 -0300

    TG-124: update login thunk and create logout

--- src/redux/auth/thunks.js
+++ src/redux/auth/thunks.js
@@ -12,10 +12,23 @@ export const login = (credentials) => {
         const {
           claims: { role }
         } = await response.user.getIdTokenResult();
-        return dispatch(loginSuccess({ role, token }));
+
+        const uid = await response.user.uid;
+        const user = await fetch(`${process.env.REACT_APP_API_URL}/employees?firebaseUid=${uid}`);
+        const userResponse = await user.json();
+        const { _id, firstName } = userResponse.data[0];
+        sessionStorage.setItem('token', token);
+        return dispatch(loginSuccess({ role, _id, firstName }));
       })
       .catch((error) => {
         return dispatch(loginError(error.toString()));
       });
+  };
+};
+
+export const logout = () => {
+  return (dispatch) => {
+    sessionStorage.removeItem('token');
+    dispatch(loginSuccess({}));
   };
 };
