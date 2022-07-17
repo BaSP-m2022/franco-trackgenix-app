b4b11bfce89da338898d76e247641084807919bf
Hernan Cazeneuve <hernancazeneuve@gmail.com>
2022-07-17 20:11:45 -0300

    TG-134: updated SA siderbar and redirected it to Admin

--- src/components/Shared/NavLink/index.js
+++ src/components/Shared/NavLink/index.js
@@ -54,12 +54,17 @@ const NavLinks = () => {
                 </li>
               </ul>
             );
-          case 'SUPERADMIN':
+          case 'SUPER-ADMIN':
             return (
               <ul className={styles.routes}>
                 <li className={styles.items}>
                   <NavLink to={'home'} className={styles.links}>
                     Home
+                  </NavLink>
+                </li>
+                <li className={styles.items}>
+                  <NavLink to={'/admins'} className={styles.links}>
+                    Admins
                   </NavLink>
                 </li>
               </ul>
