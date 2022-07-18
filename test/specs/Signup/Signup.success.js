53b0956d45dbfca0e6a288bad964fa8f2a059870
Matias Vadala <vadalamati@gmail.com>
2022-07-18 00:32:23 -0300

    BaSP-2022: now, employee dni has relation whit dob

--- test/specs/Signup/Signup.success.js
+++ test/specs/Signup/Signup.success.js
@@ -1,12 +1,12 @@
 const Signup = require('../../pageobjects/signup');
-const { name, lastname, randomDate, randomDni, randomEmail } = require('./Random.data');
+const { name, lastname, dateToReturn, randomDni, randomEmail } = require('./Random.data');
 
 beforeAll('Open Browser', () => {
   browser.url('https://franco-trackgenix-app.vercel.app/signup');
 });
 
 describe('Complete the signup inputs with valid data', () => {
-  it('Edit an admin success', async () => {
-    await Signup.signup(name, lastname, randomDate(), randomDni(), randomEmail(), 'test1234');
+  it('Create a user', async () => {
+    await Signup.signup(name, lastname, dateToReturn, randomDni(), randomEmail(), 'test1234');
   });
 });
